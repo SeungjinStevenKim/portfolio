87d59a027ef7f34008b4685571e0975a5ffe0fa5
seungjin kim <kim.sjin1221@gmail.com>
2025-08-21 11:09:42 -0600

    Simplify React Router and Vercel config for admin page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,28 +1,21 @@
 // client/src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
 import Admin from "./pages/Admin.jsx";
 import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <Home /> },
-    ],
-  },
-  {
-    path: "/admin",
-    element: <Admin />,
-  },
-]);
-
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
